2ddfc9ecf16dd6a1c00f0b55d77812b48f847ace
Luke Sneeringer <lukesneeringer@gmail.com>
2012-04-05 16:49:29 -0500

    Updated setup.py file.

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from electro import version
+from setuptools import setup
 
 setup(
-    name='Django Jinja Middleware',
+    name='django_jinja_middleware',
     version='1.0',
-    author='Luke Sneeringer <lukesneeringer@gmail.com>',
+    author='Luke Sneeringer',
+    author_email='lukesneeringer@gmail.com',
+    url='http://github.com/lukesneeringer/django-jinja-mw/',
     
+    # what to install
     packages=['django_jinja'],
+    
+    # searches and classifications
+    keywords='django,jinja,middleware',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    
+    # dependencies
+    install_requires=[
+        'django >= 1.4',
+        'jinja2 >= 2.6',
+    ],
 )
