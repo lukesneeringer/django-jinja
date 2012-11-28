3ca9df0392e364e87c33ef79354d7bd80e61b382
Luke Sneeringer <luke@sneeringer.com>
2012-11-28 16:47:31 -0500

    Jinja environments create on the fly.
    This commit solves a problem running Django tests. Some of the Django
    tests alter TEMPLATE_DIRS, and if that test runs first, it remains
    stuck in the Jinja environment.
    
    Creating the environment on the fly each time means that the settings
    are checked anew each time a JinjaTemplateResponse is created.

--- django_jinja/response.py
+++ django_jinja/response.py
@@ -8,65 +8,74 @@ from django.template.response import TemplateResponse
 import jinja2
 
 
-# set up the jinja template loaders
-loaders = []
-for path in getattr(settings, 'TEMPLATE_DIRS', []):
-    loaders.append(jinja2.FileSystemLoader(path))
-for app in getattr(settings, 'INSTALLED_APPS', []):
-    loaders.append(jinja2.PackageLoader(app))
-
-
-# create sensible defaults for the environment
-env_kwargs = {
-    'extensions': ['jinja2.ext.loopcontrols'],
-    'line_comment_prefix': '# ',
-    'loader': jinja2.ChoiceLoader(loaders),
-    'trim_blocks': True,
-}
-
-# now, accept addendums to the environment from
-# the Django settings...
-env_kwargs.update(getattr(settings, 'JINJA_ENVIRONMENT', {}))
-
-# okay, now create the jinja environment
-env = jinja2.Environment(**env_kwargs)
-
-
-# now set the globals and filters into the jinja
-# environment...
-env.globals = {
-    'url': reverse,
-    'range': xrange,
-}
-env.globals.update(getattr(settings, 'JINJA_GLOBALS', {}))
-
-for f in ('capfirst', 'linebreaks', 'linebreaksbr', 'linenumbers', 'pluralize', 'removetags', 'slugify', 'striptags', 'timesince', 'timeuntil', 'title', 'truncatewords', 'truncatewords_html', 'unordered_list', 'urlize', 'urlizetrunc', 'yesno'):
-    env.filters[f] = getattr(defaultfilters, f)
-env.filters['format_date'] = defaultfilters.date
-env.filters.update(getattr(settings, 'JINJA_FILTERS', {}))
-
-
-
 class JinjaTemplateResponse(TemplateResponse):
     """Subclass of TemplateResponse that routes its response
     through the Jinja templating language rather than the Django
     stock templating language."""
+
+    @property
+    def _environment(self):
+        # sanity check: if I already have a jinja environment object
+        # here, then I don't need a new one
+        if hasattr(self, '_jinja_env_object'):
+            return self._jinja_env_object
+
+        # set up the jinja template loaders
+        loaders = []
+        for path in getattr(settings, 'TEMPLATE_DIRS', []):
+            loaders.append(jinja2.FileSystemLoader(path))
+        for app in getattr(settings, 'INSTALLED_APPS', []):
+            loaders.append(jinja2.PackageLoader(app))
+
+        # create sensible defaults for the environment
+        env_kwargs = {
+            'extensions': ['jinja2.ext.loopcontrols'],
+            'line_comment_prefix': '# ',
+            'loader': jinja2.ChoiceLoader(loaders),
+            'trim_blocks': True,
+        }
+
+        # now, accept addendums to the environment from
+        # the Django settings...
+        env_kwargs.update(getattr(settings, 'JINJA_ENVIRONMENT', {}))
+
+        # okay, now create the jinja environment
+        env = jinja2.Environment(**env_kwargs)
+
+
+        # now set the globals into the jinja environment...
+        env.globals = {
+            'url': reverse,
+            'range': xrange,
+        }
+        env.globals.update(getattr(settings, 'JINJA_GLOBALS', {}))
+
+        # now do the same for filters...
+        for f in ('capfirst', 'linebreaks', 'linebreaksbr', 'linenumbers', 'pluralize', 'removetags', 'slugify', 'striptags', 'timesince', 'timeuntil', 'title', 'truncatewords', 'truncatewords_html', 'unordered_list', 'urlize', 'urlizetrunc', 'yesno'):
+            env.filters[f] = getattr(defaultfilters, f)
+        env.filters['format_date'] = defaultfilters.date
+        env.filters.update(getattr(settings, 'JINJA_FILTERS', {}))
+
+        # replace my property with the item itself, making the environment lazy load
+        # the first time it's called, but pulling the same object thereafter
+        self._jinja_env_object = env
+        return env
     
     def resolve_context(self, context):
         if context:
-            context = dict(context)
+            self.context = dict(context)
         else:
-            context = {}
+            self.context = {}
         
         # I still want to keep the use of my Django context processors
         # even though this is a Jinja template; therefore, process them manually
         for context_processor in get_standard_processors():
             new_stuff = context_processor(self._request)
             if new_stuff:
-                context.update(dict(new_stuff))
+                self.context.update(dict(new_stuff))
     
         # return a flat dict; jinja doesn't have context objects
-        return context
+        return self.context
     
     def resolve_template(self, template):
         """Takes a template and tries to return back the appropriate
@@ -74,8 +83,8 @@ class JinjaTemplateResponse(TemplateResponse):
         If an explicit Django template object is passed, do nothing."""
         
         if isinstance(template, basestring):
-            return env.get_template(template)
+            return self._environment.get_template(template)
         elif isinstance(template, (list, tuple)):
-            return env.select_template(template)
+            return self._environment.select_template(template)
         else:
             return template
